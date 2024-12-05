fd6e8fa567b436f39853cd5d1d6c7fd49b0f558d
Yimeng Qin <yimengq@stanford.edu>
2024-12-05 14:05:17 -0800

    proj3

---
+++ proj3/ppg.py
@@ -0,0 +1,201 @@
+# import time
+# import sys
+# import ADS1256
+# import spidev
+# import RPi.GPIO as GPIO
+# import matplotlib
+# matplotlib.use("Agg")  # Use non-interactive backend suitable for headless environments
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from scipy.signal import find_peaks, correlate
+# from matplotlib.patches import Ellipse
+
+# SETUP_SPI = False 
+# SET_CHANNEL = False
+# FREQUENCY = 500
+# GPIO_FREQ_FACTOR = 1
+# sense_pin = 10
+
+# class FrequencyDetector:
+#     def __init__(self):
+#         self.current_time = time.time()
+#         self.last_time = self.current_time
+#         self.start_time = self.current_time
+#         self.count = 0
+        
+#     def update(self):
+#         self.count += 1
+#         self.current_time = time.time()
+        
+#     def realtime_update(self):
+#         self.last_time = self.current_time
+#         self.current_time = time.time()
+#         self.count += 1
+#         dt = self.current_time - self.last_time
+#         if dt > 0:
+#             freq = 1 / dt
+#             sys.stdout.write("\r")
+#             sys.stdout.write(f"{freq:.4f} Hz       ")
+#             sys.stdout.flush()
+
+#     def get_freq(self):
+#         return self.count / (self.current_time - self.start_time)
+    
+# class FrequencyRegulator:
+#     """Class to regulate frequency of GPIO output."""
+#     def __init__(self, frequency):
+#         self.frequency = frequency
+#         self.start_time = time.time()
+#         self.dt = 1 / self.frequency
+#         self.next_pulse_time = self.start_time + self.dt
+    
+#     def is_next_pulse_ok(self):
+#         if time.time() > self.next_pulse_time:
+#             self.next_pulse_time += self.dt
+#             return True
+#         return False
+
+# def main():
+#     # GPIO setup
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+    
+#     # ADC setup
+#     ADC = ADS1256.ADS1256()
+#     ADC.ADS1256_init()
+    
+#     # SPI setup
+#     if SETUP_SPI:
+#         SPI = spidev.SpiDev(0, 0)
+#         SPI.mode = 0b01
+#         SPI.max_speed_hz = 3000000
+#     ADC.ADS1256_SetChannal(sense_pin)
+#     ADC.ADS1256_WriteCmd(0xFC)  # sync
+#     ADC.ADS1256_WriteCmd(0x00)  # wakeup
+#     adc_values = []
+
+#     freq_detector = FrequencyDetector()
+#     freq_regulator = FrequencyRegulator(FREQUENCY)
+
+#     while(len(adc_values) < 500): 
+        
+#         if not freq_regulator.is_next_pulse_ok():
+#             continue
+#         freq_detector.update()    
+           
+#         adc_value = ADC.ADS1256_Read_ADC_Data() * 5.0 / 0x7fffff
+#         adc_values.append(adc_value)
+#         print(adc_value)
+
+#     print(f"Frequency: {freq_detector.get_freq():.4f} Hz")
+
+# if __name__ == "__main__":
+#     main()
+
+
+import time
+import sys
+import ADS1256
+import spidev
+import RPi.GPIO as GPIO
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Use non-interactive backend
+matplotlib.use("Agg")
+
+SETUP_SPI = False
+SET_CHANNEL = False
+FREQUENCY = 500
+GPIO_FREQ_FACTOR = 1
+sense_pin = 10
+
+class FrequencyDetector:
+    def __init__(self):
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        self.start_time = self.current_time
+        self.count = 0
+        
+    def update(self):
+        self.count += 1
+        self.current_time = time.time()
+        
+    def get_freq(self):
+        return self.count / (self.current_time - self.start_time)
+
+class FrequencyRegulator:
+    """Class to regulate frequency of GPIO output."""
+    def __init__(self, frequency):
+        self.frequency = frequency
+        self.start_time = time.time()
+        self.dt = 1 / self.frequency
+        self.next_pulse_time = self.start_time + self.dt
+    
+    def is_next_pulse_ok(self):
+        if time.time() > self.next_pulse_time:
+            self.next_pulse_time += self.dt
+            return True
+        return False
+
+def main():
+    # GPIO setup
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    
+    # ADC setup
+    ADC = ADS1256.ADS1256()
+    ADC.ADS1256_init()
+    
+    # SPI setup
+    if SETUP_SPI:
+        SPI = spidev.SpiDev(0, 0)
+        SPI.mode = 0b01
+        SPI.max_speed_hz = 3000000
+    ADC.ADS1256_SetChannal(sense_pin)
+    ADC.ADS1256_WriteCmd(0xFC)  # sync
+    ADC.ADS1256_WriteCmd(0x00)  # wakeup
+
+    timesteps = []
+    adc_values = []
+    # adc_gain = 2
+    freq_detector = FrequencyDetector()
+    freq_regulator = FrequencyRegulator(FREQUENCY)
+
+    # Infinite loop
+    try:
+        while True:
+            if not freq_regulator.is_next_pulse_ok():
+                continue
+        
+            freq_detector.update()
+            # adc_value = adc_gain* (ADC.ADS1256_Read_ADC_Data() * 5.0 / 0x7fffff)
+            adc_value = ADC.ADS1256_Read_ADC_Data() * 5.0 / 0x7fffff
+            adc_values.append(adc_value)
+            timesteps.append(time.time())
+            print(adc_value)
+
+            # # Keep only the last 500 samples for plotting
+            # if len(adc_values) > 500:
+            #     adc_values.pop(0)
+
+            # Save plot periodically (every 100 readings)
+            # if len(adc_values) % 100 == 0:
+            #     plt.figure(figsize=(10, 5))
+            #     plt.plot(adc_values, label="ADC Values")
+            #     plt.title("ADC Voltage Values")
+            #     plt.xlabel("Sample")
+            #     plt.ylabel("Voltage (V)")
+            #     # plt.ylim(-5, 5)  # Set Y-axis limits to ADC range
+            #     plt.legend()
+            #     plt.grid()
+            #     plt.savefig("adc_plot.png")  # Save the plot as an image file
+            #     print("Plot updated and saved as adc_plot.png")
+                
+    except KeyboardInterrupt:
+        print(f"Frequency: {freq_detector.get_freq():.4f} Hz")
+        np.savez('ppg.npz', {'adc':np.array(adc_values), 'time':np.array(timesteps)})
+
+if __name__ == "__main__":
+    main()
---
+++ proj3/ppg_fft.py
@@ -0,0 +1,40 @@
+import numpy as np
+from numpy import blackman
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.signal import butter, filtfilt
+# Use non-interactive backend
+matplotlib.use("Agg")
+
+def blackman_window(_fft_size, signal):
+    window = blackman(_fft_size)
+    signal_padded = np.zeros(_fft_size)
+    signal_padded[:len(signal)] = signal[:min(len(signal), _fft_size)]
+    windowed_signal = signal_padded * window
+    return windowed_signal
+
+def fft(signal,_fft_size,f_sampling):
+    window = blackman(_fft_size)
+    signal_padded = np.zeros(_fft_size)
+    signal_padded[:len(signal)] = signal[:min(len(signal), _fft_size)]
+    windowed_signal = signal_padded * window
+    fft_output = np.fft.fft(windowed_signal[:_fft_size])
+    fft_freqs = np.fft.fftfreq(_fft_size, 1.0 / f_sampling)
+    return fft_freqs[:_fft_size//2], np.abs(fft_output[:_fft_size//2])
+
+
+adc = np.load('ppg.npz', allow_pickle=True)['arr_0'].item()['adc']
+time = np.load('ppg.npz', allow_pickle=True)['arr_0'].item()['time']
+
+time_diffs = np.diff(time) 
+sampling_rates = 1 / time_diffs  
+avg_sampling_rate = np.mean(sampling_rates)
+
+print(f"Average Sampling Rate: {avg_sampling_rate:.2f} Hz")
+
+freq, adc_fft = fft(adc, 4096, 500)
+
+plt.clf()
+plt.plot(freq, 20 * np.log10(adc_fft+1e-10))
+plt.xlim([0,100])
+plt.savefig('fft.png')
